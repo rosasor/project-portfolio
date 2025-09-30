ac7e5fa3d2ed3560f7fc7b55ae56fa40f6fcb4e4
rosasor <elenarosaw@gmail.com>
2025-09-30 11:14:42 -0400

    added more padding-y to nav bar, i didnt realize it was so skinnnny

--- my-portfolio/src/components/NavigationMenu.tsx
+++ my-portfolio/src/components/NavigationMenu.tsx
@@ -17,7 +17,7 @@ const NavBar = () => {
 
   return (
     <div className="fixed top-[1.5rem] mx-auto left-1/2 transform -translate-x-1/2 z-10">
-      <NavigationMenu.Root className="flex bg-background/80 backdrop-blur-sm rounded-full px-5 py-1 border-background/50 shadow-md shadow-neutral-950/5">
+      <NavigationMenu.Root className="flex bg-background/80 backdrop-blur-sm rounded-full px-5 py-3 border-background/50 shadow-md shadow-neutral-950/5">
         <NavigationMenu.List className="flex gap-8 items-center">
           {links.map((link) => {
             const isActive =
@@ -30,7 +30,7 @@ const NavBar = () => {
                 <motion.div>
                 <Link
                   href={link.href}
-                  className={`font-medium text-[.9rem] py-2 transition-colors
+                  className={`font-medium text-[.9rem] py-3 transition-colors
                     ${
                       isActive
                         ? "text-black dark:text-white"
