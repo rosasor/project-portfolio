328467872c049732b454edbd9fd2ec41c2445de5
rosasor <elenarosaw@gmail.com>
2025-10-06 16:57:46 -0400

    confused manhua and manhwa... and rewrote sections

--- my-portfolio/src/components/AboutDetails.tsx
+++ my-portfolio/src/components/AboutDetails.tsx
@@ -7,8 +7,11 @@ const AboutDetails = () => {
             <Flex className="flex-col gap-12">
                 <Flex className="flex-col">
                     <Text className=" text-neutral-700 dark:text-neutral-200">
-                        Hi&#x2c; I&apos;m <Strong>Elena Rosa Wheelen</Strong>&#x2c; a front&ndash;end developer based in South Florida&#46;
-                        I&apos;m a human being&#46;
+                        Hi&#x2c; I&apos;m <Strong>Elena Rosa Wheelen</Strong>&#x2c; a front&ndash;end developer based in South Florida who loves making everyday life a 
+                        little easier&#46; I&apos;m driven by curiosity and creativity&#x2c; often building projects that start as simple ideas 
+                        or personal challenges&#46; Each project is a chance to learn&#x2c; experiment&#x2c; and create something meaningful&#46;
+
+
                     </Text>
                 </Flex>
                 <Flex className="flex-col">
@@ -23,11 +26,22 @@ const AboutDetails = () => {
                 </Flex>
                 <Flex className="flex-col">
                     <Heading className="mb-4 font-bold text-xl sm:text-2xl text-neutral-800 dark:text-neutral-200">
+                        How I Learn
+                    </Heading>
+                    <Text className=" text-neutral-700 dark:text-neutral-200">
+                    Whenever I&apos;m learning something new&#x2c; I like to jump right in and experiment&#46;
+                    I use a mix of resources like documentation&#x2c; tutorials&#x2c; templates&#x2c; and LLM chats to understand new concepts and 
+                    grow my skills&#46; I also believe in learning from others and often reach out to both peers and mentors to hear their insights&#x2c;
+                     because collaboration and curiosity go hand in hand&#46;
+                    </Text>
+                </Flex>
+                <Flex className="flex-col">
+                    <Heading className="mb-4 font-bold text-xl sm:text-2xl text-neutral-800 dark:text-neutral-200">
                         Behind the Pixels
                     </Heading>
                     <Text className=" text-neutral-700 dark:text-neutral-200">
                         In my downtime&#x2c; I enjoy learning something new&#x2c; working out&#x2c; 
-                        playing video games with friends&#x2c; or reading manhua&#46;
+                        playing video games with friends&#x2c; or reading manhwa&#46;
                     </Text>
                 </Flex>
                 <Flex className="flex-col">
